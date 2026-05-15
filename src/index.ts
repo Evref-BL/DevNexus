68ca19cbf956ee9a0ca1c86873ef5b7c1f172241
Gabriel-Darbord <gabriel.darbord@berger-levrault.com>
2026-05-15 18:31:07 +0200

    Add forge work tracking providers

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+export * from "./workTrackingGitHubProvider.js";
+export * from "./workTrackingGitLabProvider.js";
+export * from "./workTrackingJiraProvider.js";
 export * from "./nexusExtension.js";
 export * from "./workTrackingLocalProvider.js";
 export * from "./workTrackingTypes.js";
